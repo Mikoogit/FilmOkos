dbd4333bf28ee3c3346b38f63ff3eef5f845f449
Mikoo <teszt@gmail.com>
2026-01-15 10:58:14 +0100

    Login Page

--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -1,6 +1,29 @@
 import React from "react";
-export default function Login(){
-    return(
+import logo from "../assets/logo.png";
+import "../styles/login.css";
+
+export default function Login() {
+  return (
+    <div className="login-page">
+      <img src={logo} alt="Logo" className="login-logo" />
+
+      <div className="login-container">
         <h1>Bejelentkezés</h1>
-    )
+
+        <label>Felhasználói név:</label>
+        <input type="text" placeholder="Ide írd a felhasználó neved" />
+
+        <label>Jelszó:</label>
+        <input type="password" placeholder="Ide írd a jelszavad" />
+
+        <a href="#" className="forgot">Elfelejtett jelszó</a>
+
+        <button className="btn primary">BEJELENTKEZÉS</button>
+
+        <span className="or">VAGY</span>
+
+        <button className="btn secondary">REGISZTRÁCIÓ</button>
+      </div>
+    </div>
+  );
 }
